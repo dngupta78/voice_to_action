e6d3f77b40ece33635d2e2b4a32b6c3dc3740ea1
deep_gupta <deep_gupta@persistent.com>
2024-02-20 22:42:29 +0530

    Developed text to audio endpoints

---
+++ backend/api/endpoints/text_to_audio.py
@@ -0,0 +1,19 @@
+from gtts import gTTS
+import os
+from fastapi import FastAPI, APIRouter
+
+router = APIRouter()
+app = FastAPI()
+
+
+@router.get("/text_to_audio/", tags=["Text To Audio"])
+def text_to_audio(text, language='en'):
+    tts = gTTS(text=text, lang=language, slow=False)
+    tts.save("output.mp3")
+    os.system("start output.mp3")
+    # os.system("mpg123 output.mp3")  # for Linux
+
+
+# Example usage:
+text = "Hello, this is a sample text to be converted into audio."
+text_to_audio(text)
