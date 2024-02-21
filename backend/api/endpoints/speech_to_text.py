1d9d979f77b6d9c51a79d063c7c1b2e16e9703fe
deep_gupta <deep_gupta@persistent.com>
2024-02-21 22:26:56 +0530

    Related with same changes from previous 2 commit

--- backend/api/endpoints/speech_to_text.py
+++ backend/api/endpoints/speech_to_text.py
@@ -1,3 +1,10 @@
+from fastapi import FastAPI, APIRouter
+import speech_recognition as sr
+
+router = APIRouter()
+app = FastAPI()
+
+
 @router.get("/speech_to_text/", tags=["Audio To Text"])
 def speech_to_text():
     # Initialize recognizer
